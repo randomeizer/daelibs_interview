69b320a3daed7276ef6fcab853be2e80b8cb83e8
David Peterson <david@randombits.org>
2024-05-30 17:43:26 +1000

    Updated global font styles to match.

--- daelibs-frontend/src/App.vue
+++ daelibs-frontend/src/App.vue
@@ -26,12 +26,15 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&display=swap');
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Open Sans', sans-serif;
+  font-size: 14px;
+  color: #5A5A5A;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
 }
 </style>
