09004ed61e35515a68a035667a8406b1f5f92b40
David Peterson <david@randombits.org>
2024-05-30 17:50:14 +1000

    Added more interesting data.

--- daelibs-frontend/src/services/dataService.js
+++ daelibs-frontend/src/services/dataService.js
@@ -1,9 +1,12 @@
 export const getData = () => {
   return [
-    { name: "Lauren Vickers", target: 90, actual: 95, points: 69 },
-    { name: "Shazia Darby", target: 80, actual: 88, points: 26 },
+    { name: "Lauren Vickers", target: 90, actual: 45, points: 69 },
+    { name: "Shazia Darby", target: 80, actual: 43, points: 26 },
     { name: "Zara Cain", target: 85, actual: 81, points: 124 },
-    { name: "Clair Newton", target: 80, actual: 22, points: 82 },
-    { name: "April Bruce Kieran", target: 90, actual: 92, points: 26 }
+    { name: "Edna Bradbury Parsons", target: 80, actual: 91, points: 26 },
+    { name: "Clair Newton", target: 90, actual: 81, points: 82 },
+    { name: "April Bruce Kieran", target: 50, actual: 60, points: 74 },
+    { name: "Timothy Bower", target: 80, actual: 85, points: 90 },
+    { name: "Tracie Atkin Stanton", target: 80, actual: 70, points: 66 }
   ];
 };
